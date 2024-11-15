2bb0c506d96f074617130afa94dfa20a9c547d47
amirali <amir.hkh77.ali@gmail.com>
2024-11-15 10:37:25 +0330

    initial commit

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,106 +1,102 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="hHh lpR fFf">
     <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+      <q-toolbar class="top">
+        <q-btn flat icon="home" to="#top" aria-label="Home" />
+        <q-space />
+        <q-tabs v-model="tab" align="center">
+          <q-tab name="home" label="HOME" to="#HOME" />
+          <q-tab name="menu" label="MENU" to="#MENU" />
+          <q-tab name="products" label="PRODUCTS" to="#PRODUCTS" />
+          <q-tab name="about" label="ABOUT" to="#ABOUT" />
+          <q-tab name="contact" label="CONTACT US" to="#CONTACT-US" />
+        </q-tabs>
+        <q-space />
+        <q-btn flat icon="person" />
+        <q-btn flat icon="shopping_basket" @click="showCart" />
       </q-toolbar>
     </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-footer class="text-center">
+      <div>تمامی حقوق این صفحه متعلق به بنده است ©</div>
+      <q-list>
+        
+      </q-list>
+    </q-footer>
   </q-layout>
 </template>
 
+
 <script setup>
-import { ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+import { ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
 
 defineOptions({
-  name: 'MainLayout'
-})
+  name: "MainLayout",
+});
 
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
+    title: "Docs",
+    caption: "quasar.dev",
+    icon: "school",
+    link: "https://quasar.dev",
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
+    title: "Github",
+    caption: "github.com/quasarframework",
+    icon: "code",
+    link: "https://github.com/quasarframework",
   },
   {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
+    title: "Discord Chat Channel",
+    caption: "chat.quasar.dev",
+    icon: "chat",
+    link: "https://chat.quasar.dev",
   },
   {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
+    title: "Forum",
+    caption: "forum.quasar.dev",
+    icon: "record_voice_over",
+    link: "https://forum.quasar.dev",
   },
   {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
+    title: "Twitter",
+    caption: "@quasarframework",
+    icon: "rss_feed",
+    link: "https://twitter.quasar.dev",
   },
   {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
+    title: "Facebook",
+    caption: "@QuasarFramework",
+    icon: "public",
+    link: "https://facebook.quasar.dev",
   },
   {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
+    title: "Quasar Awesome",
+    caption: "Community Quasar projects",
+    icon: "favorite",
+    link: "https://awesome.quasar.dev",
+  },
+];
 
-const leftDrawerOpen = ref(false)
+const leftDrawerOpen = ref(false);
 
-function toggleLeftDrawer () {
-  leftDrawerOpen.value = !leftDrawerOpen.value
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
 }
 </script>
+
+
+<style>
+.top {
+  display: flex;
+  justify-content: space-between;
+  height: 100px;
+  background-color: black;
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,158 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <q-page class="flex flex-center bg-darkk text-white" >
+    <q-card class="text-center q-pa-md card" flat bordered >
+      <div class="box1">
+        <q-card-section>
+        <h2 class="text-h2 text-weight-bold">Start your day <br> with a black <span class="text-orange">coffee</span></h2>
+        <p class="text-grey-5 text-weight-medium">
+          Choose and taste delicious coffee from the best beans
+        </p>
+      </q-card-section>
+      <q-card-actions  align="center">
+        <q-btn class="q-ml-mdb" label="Order Now" to="#PRODUCTS" style="background-color: #CB863F; width: 150px;" />
+        <q-btn outline  label="Learn About Us" to="#ABOUT" class="q-ml-md" style="color: #6C4620; width: 150px;" />
+      </q-card-actions>
+      </div>
+      <q-img src="public/icons/images/cup.png" class="pic"/>
+    </q-card>
+
   </q-page>
+
+
+<div class="container">
+  <div class="q-pa-md row justify-center container-c">
+  <q-card v-for="product in topics.populars" :key="product.name" class="My-card ">
+    <q-img :src="`public/icons/images/${product.pic}.jpg`" class="rounded-borders" />
+    <q-card-section>
+      <div class="text-h5 text-weight-medium">{{ product.name }}</div>
+      <div class="text-subtitle1 text-grey text-overline">{{ product.des }}</div>
+      <div class="text-subtitle2  text-orange-7">${{ product.price }}</div>
+    </q-card-section>
+    <q-card-actions align="left">
+      <q-btn icon="add_shopping_cart"  label="Add to Cart" class="addC" style="background-color: #CB863F;"/>
+    </q-card-actions>
+  </q-card>
+  <!-- <q-space></q-space> -->
+</div>
+<div class="q-pa-md row justify-center container-c">
+  <q-card v-for="product in topics.bestSellers" :key="product.name" class="My-card ">
+    <q-img :src="`public/icons/images/${product.pic}.jpg`" class="rounded-borders" />
+    <q-card-section>
+      <div class="text-h5 text-weight-medium">{{ product.name }}</div>
+      <div class="text-subtitle1 text-grey text-overline ">{{ product.des }}</div>
+      <div class="text-subtitle2 text-orange-7">${{ product.price }}</div>
+    </q-card-section>
+    <q-card-actions align="left">
+      <q-btn icon="add_shopping_cart"  label="Add to Cart"  class="addC" style="background-color: #CB863F;"/>
+    </q-card-actions>
+  </q-card>
+</div>
+</div>
+
 </template>
 
-<script setup>
-defineOptions({
-  name: 'IndexPage'
-});
+
+<script>
+import { ref, reactive } from 'vue'
+
+export default {
+  setup() {
+    const primary = "#CB863F"
+    const basket = ref(JSON.parse(localStorage.getItem('basket')) || [])
+    const topics = reactive({
+      populars: [
+        { name: 'Esperesso Coffee', des: 'with milk', price: 5.0, pic: 'E' },
+        { name: 'Latte Coffee', des: 'with milk', price: 7.5, pic: 'L' },
+        { name: 'Cappuccino Coffee', des: 'with chocolate', price: 6.2, pic: 'C' },
+      ],
+      bestSellers: [
+        { name: 'Americano Coffee', des: 'with milk', price: 10.3, pic: 'A' },
+        { name: 'Macchiato Coffee', des: 'with cream', price: 8.5, pic: 'M' },
+        { name: 'Mocha', des: 'with oat cream', price: 11.2, pic: 'MC' }
+      ],
+    })
+
+    const showCart = () => { /* logic to show cart */ }
+    const addToCart = (product) => {
+      // logic to add product to basket
+    }
+
+    return { basket, topics, showCart, addToCart }
+  }
+}
 </script>
+
+<style>
+:root {
+  --bg-color: #000000;
+  --btn-color: #CB863F;
+  --border-color: #6C4620;
+  --white: #ffffff;
+  --text-gray: #9E9E9E;
+}
+
+body {
+  background-color: var(--bg-color);
+  font-family: "Poppins", sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+.container-c {
+  width: 1400px;
+  display: flex;
+  gap: 100px;
+}
+
+.My-card {
+  /* width: 260px; */
+  border-radius: 8px;
+  background-color: var(--bg-gray);
+  color: var(--white);
+  border: 3px solid var(--border-color);
+
+}
+
+.text-orange-7 {
+  color: var(--btn-color);
+}
+
+.rounded-borders {
+  border-radius: 50%;
+  border: 3px solid var(--border-color);
+  width: 260px;
+  height: 260px;
+}
+
+.addC {
+  font-size: 12px;
+
+}
+
+.text-orange {
+  color: #CB863F;
+}
+
+.bg-darkk {
+  background-color: #000000;
+}
+
+.text-grey-5 {
+  color: #9E9E9E;
+}
+
+.card {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: none;
+  width: 1400px;
+}
+
+.box1 q-btn {
+  text-align: center;
+}
+.pic {
+  width: 800px;
+}
+</style>
